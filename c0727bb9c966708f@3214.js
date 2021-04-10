1c71f3d46ba6772791753dd9a348a13647c12148
imbottlebird <imbottlebird@gmail.com>
2021-04-11 02:25:35 +0900

    update

--- c0727bb9c966708f@3214.js
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<iframe width="100%" height="630" frameborder="0"
-  src="https://observablehq.com/embed/c0727bb9c966708f?cells=chart"></iframe>
-  </body>
-  </html>
--- index.js
+++
@@ -1,1 +0,0 @@
-export {default} from "./c0727bb9c966708f@3214.js";
